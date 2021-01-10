1cd115f4ea0c5c01ac3d40cff85bb68b158ba0c9
taicw <taicw@163.com>
2021-01-11 07:25:47 +0800

    配置gateway网关过滤器。单一过滤器，全局过滤器

--- cloud-gateway-gateway9527/src/main/java/com/taicw/springcloud/gateway/GatewayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/taicw/springcloud/gateway/GatewayMain9527.java
@@ -2,14 +2,14 @@ package com.taicw.springcloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author taichangwei
  */
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayMain9527 {
     public static void main(String[] args) {
         SpringApplication.run(GatewayMain9527.class, args);
